862854484e8afad42ffbfda62a602c580ae602fb
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-11-24 02:07:18 +0000

    add fn to get rename and delete directory

--- directory/service.ts
+++ directory/service.ts
@@ -17,3 +17,25 @@ export async function createDirectory(client: PrismaClient, name: Directory["nam
 
     return directory
 }
+ 
+export async function getDirectory(client: PrismaClient, id: Directory["id"]): Promise<Directory | null> {
+    return await client.directory.findUnique({ where: { id }, include: { files: true, directories: true}})
+}
+
+export async function renameDirectory(client: PrismaClient, id: Directory["id"], name: Directory["name"]): Promise<Directory | null> {
+    if (name.toLowerCase() === 'root') {
+        throw new Error("Directory 'root' is reserved")
+    }
+
+    const directory = await client.directory.findUnique({ where: { id }});
+
+    if (directory?.name  === "root") {
+        throw new Error("Root directory may not be renamed")
+    }
+    return await client.directory.update({ where: { id }, data: { name }, include: { files: true, directories: true } })
+}
+
+export async function deleteDirectory (client: PrismaClient, id: Directory["id"]): Promise<boolean> {
+    await client.directory.delete({ where: { id }})
+    return true;
+}
